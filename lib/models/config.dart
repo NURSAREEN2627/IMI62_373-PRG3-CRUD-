85231bbe4c5a6b0892b650263de132332c3bf12f
NURSAREEN2627 <120258174+NURSAREEN2627@users.noreply.github.com>
2023-08-31 19:58:53 +0700

    Initial commit

---
+++ lib/models/config.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_application_11/models/users.dart';
+
+// ignore: camel_case_types
+class configure {
+  static const server = "10.116.1.124:3000";
+  static Users login = Users();
+  static List<String> gender = ["None", "male", "female"];
+}
---
+++ lib/screen/sidemenu.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_11/screen/home.dart';
+import 'package:flutter_application_11/screen/login.dart';
+
+import '../models/config.dart';
+import '../models/users.dart';
+
+class SideMenu extends StatelessWidget {
+  const SideMenu({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    String accountName = "N/A";
+    String accountEmail = "N/A";
+    String accountUrl ="https://as2.ftcdn.net/v2/jpg/02/02/62/31/1000_F_202623148_qF2K0DyHX1hJ5aaATuyD9FAkbWCXfBYM.jpg";
+
+    Users user = configure.login;
+
+    if (user.id != null) {
+      accountName = user.fullname!;
+      accountEmail = user.email!;
+    }
+    return Drawer(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          UserAccountsDrawerHeader(
+            accountName: Text(accountName),
+            accountEmail: Text(accountEmail),
+            currentAccountPicture: CircleAvatar(
+              backgroundImage: NetworkImage(accountUrl),
+              backgroundColor: Colors.white,
+            ),
+          ),
+          ListTile(
+            leading: Icon(Icons.home),
+            title: Text('Home'),
+            onTap: () {
+              Navigator.pushNamed(context, Home.routeName);
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.lock),
+            title: Text('Login'),
+            onTap: () {
+              Navigator.pushNamed(context, Login.routeName);
+            },
+          )
+        ],
+      ),
+    );
+  }
+}
